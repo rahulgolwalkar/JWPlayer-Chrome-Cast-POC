8a7d54f5cbea9d7f5938319e308641b737f04698
Rahul Golwalkar <r.golwalkar@onefc.com>
2019-05-08 16:38:17 +0530

    Initial Commit

---
+++ JWPlayer POC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JWPlayer POC
+//
+//  Created by Rahul Golwalkar  on 08/05/19.
+//  Copyright © 2019 ONE Championship. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
